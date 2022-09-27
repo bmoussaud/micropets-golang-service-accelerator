8010f1b238e27451f0a5552892d8619b53267520
Benoit Moussaud <benoit@moussaud.org>
2022-09-27 15:57:17 +0200

    fix the service to start without any default values

--- internal/config.go
+++ internal/config.go
@@ -8,15 +8,15 @@ import (
 	"github.com/spf13/viper"
 )
 
-//Config Structure
+// Config Structure
 type Config struct {
 	Service struct {
-		Port   string
-		Listen bool
-		Mode string
+		Port           string
+		Listen         bool
+		Mode           string
 		FrequencyError int
-		Delay  struct {
-			Period int
+		Delay          struct {
+			Period    int
 			Amplitude float64
 		}
 		From string
@@ -37,13 +37,16 @@ type Config struct {
 }
 
 var GlobalConfig Config
-	
-//LoadConfiguration method
+
+// LoadConfiguration method
 func LoadConfiguration() Config {
 	if !GlobalConfig.setup {
 		viper.SetConfigType("json")
 		viper.SetEnvPrefix("mp")           // will be uppercased automatically eg. MP_OBSERVABILITY.TOKEN=$(TO_TOKEN)
 		viper.SetConfigName("pets_config") // name of config file (without extension)
+
+		GlobalConfig.Service.Port = ":8080"
+		GlobalConfig.Service.Listen = true
 		viper.AutomaticEnv()
 
 		if serviceConfigDir := os.Getenv("SERVICE_CONFIG_DIR"); serviceConfigDir != "" {
@@ -52,20 +55,27 @@ func LoadConfiguration() Config {
 
 		}
 		//add default config path
-		viper.AddConfigPath("/config/")  // path to look for the config file in
+		viper.AddConfigPath("/config/")         // path to look for the config file in
 		viper.AddConfigPath("/etc/micropets/")  // path to look for the config file in
 		viper.AddConfigPath("$HOME/.micropets") // call multiple times to add many search paths
 		viper.AddConfigPath(".")                // optionally look for config in the working directory
 
-		err := viper.ReadInConfig() // Find and read the config file
-		if err != nil {             // Handle errors reading the config file
-			panic(fmt.Errorf("fatal error config file: %s ", err))
+		if err := viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				// Config file not found; ignore error if desired
+				fmt.Printf("Config file not found, %v", err)
+			} else {
+				fmt.Printf("Config file was found but another error was produced, %v", err)
+			}
+		} else {
+			// Config file found and successfully parsed
+
+			err := viper.Unmarshal(&GlobalConfig)
+			if err != nil {
+				panic(fmt.Errorf("unable to decode into struct, %v", err))
+			}
 		}
 
-		err = viper.Unmarshal(&GlobalConfig)
-		if err != nil {
-			panic(fmt.Errorf("unable to decode into struct, %v", err))
-		}
 		GlobalConfig.setup = true
 		fmt.Printf("Resolved Configuration\n")
 		fmt.Printf("%+v\n", GlobalConfig)
